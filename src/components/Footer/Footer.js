a226a1eba30ab800625503b50131ccb493d2f2b7
itsvitaliio <itsvitaliio@gmail.com>
2021-07-12 14:06:21 -0400

    Deploy fcc project to github pages

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => (
+  <footer className="Footer">
+    <span>
+      Proudly made by @itsvitaliio
+    </span>
+  </footer>
+);
+
+export default Footer;
